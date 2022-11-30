26228b3fe2f2704e010dac203d70d3518b600e28
Alexey Sokolov <alexvyber@gmail.com>
2022-11-30 18:05:36 +0300

    satisfies

---
+++ satisfies/src/satisfies.ts
@@ -0,0 +1,16 @@
+type Colors = "red" | "green" | "blue"
+type RGB = [red: number, green: number, blue: number] 
+
+const palette = {
+  red: [255, 0, 0],
+  green: "#00ff00",
+  blue: [0, 0, 255]
+} satisfies Record<Colors, string | RGB>
+
+// We want to be able to use array methods on 'red'...
+const redComponent = palette.red.at(0)
+// const blueComponent = palette.blue.at(0)
+
+// or string methods on 'green'...
+const greenNormalized = palette.green.toUpperCase()
+
