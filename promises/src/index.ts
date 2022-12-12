25cb75a916e2fd3aa3762236cd537b4c72eb5304
Alexey Sokolov <alexvyber@gmail.com>
2022-12-12 16:52:56 +0300

    promises

---
+++ promises/src/index.ts
@@ -0,0 +1,14 @@
+const one = new Promise<string>(resolve => {
+  resolve("text")
+})
+
+let oneRes: string
+
+one.then(value => {
+  setTimeout(() => {
+    oneRes = value
+  }, 1000)
+})
+
+console.log("🚀 ~ one ~ one", await one)
+console.log("🚀 ~ oneRes ~ oneRes", oneRes)
