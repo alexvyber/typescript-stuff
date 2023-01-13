5ddcdd16ee67794e16745e50c18020f75b83a98f
Alexey Sokolov <alexvyber@gmail.com>
2023-01-13 22:29:32 +0300

    reg

---
+++ reg/src/number-methods.ts
@@ -0,0 +1,19 @@
+// number-methods.ts
+
+console.log(69..toString()) // "69"
+console.log(13.123123.toFixed())  // 13
+console.log(666..toExponential()) // 6.66e+2
+
+// number-methods.ts
+
+// ❗ syntax error 
+// 👇 will no work 
+console.log(13.123123..toFixed()) 
+
+
+// number-methods.ts
+
+console.log((69).toString()) // "69"
+console.log((13.123123).toFixed()) // 13
+console.log((666).toExponential()) // 6.66e+2
+
