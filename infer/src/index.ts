2ebb449577c735865b1a1a67f49abc7cbf262771
Alexey Sokolov <alexvyber@gmail.com>
2023-02-04 15:59:45 +0300

    infer: new content

---
+++ infer/src/index.ts
@@ -0,0 +1,12 @@
+type Some<Type> = Type extends {
+  [key in keyof Type as Type extends string ? string : never]: infer PropType
+}
+  ? PropType
+  : never
+
+type Obj = {
+  some: string
+  other: number
+}
+
+type Result = Some<Obj>
