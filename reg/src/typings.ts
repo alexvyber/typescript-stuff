34ecde7264ca59675936d011a691be4775450d9a
Alexey Sokolov <alexvyber@gmail.com>
2023-02-06 13:08:03 +0300

    new stuff

---
+++ reg/src/typings.ts
@@ -0,0 +1,16 @@
+type Enum_Order_Mass = "kg1" | "kg5" | "kg10" | "kg15"
+
+type GetMassValueFromKey<
+  Type,
+  Part extends string
+> = Type extends `${Part}${infer One}` ? One : Type
+type Mass = {
+  [Key in Enum_Order_Mass]: `${GetMassValueFromKey<Key, "kg">} кг`
+}
+
+const massObj: Mass = {
+  kg1: "1 кг",
+  kg5: "5 кг",
+  kg10: "10 кг",
+  kg15: "15 кг"
+}
