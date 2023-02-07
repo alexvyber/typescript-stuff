c292615332bc6d3a61d5ee84e210bcab345f4262
Alexey Sokolov <alexvyber@gmail.com>
2023-02-07 21:41:51 +0300

    using classes

---
+++ reg/src/methods-on-types.ts
@@ -0,0 +1,34 @@
+export {}
+
+type Some = {
+  name: string
+  printName: () => void
+  callMethod: () => void
+}
+
+class SomeClass implements Some {
+  name
+  method
+
+  constructor({ name, method }: { name: string; method: (arg: any) => void }) {
+    this.name = name
+    this.method = method
+  }
+
+  callMethod() {
+    this.method(this.name)
+  }
+
+  printName() {
+    return this.name
+  }
+}
+
+const some = new SomeClass({
+  name: process.argv[2],
+  method: (arg: any) => console.log("arg!!!!!!!!!!!!!!!!" + arg)
+})
+
+console.log("🚀 ~ some.callMethod()", some.callMethod())
+console.log("🚀 ~ some.name", some.name)
+// console.log("🚀 ~ some.printName()", some.printName())
