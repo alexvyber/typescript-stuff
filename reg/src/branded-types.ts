2ebb93a75efac24267a4443ab83c28c4f66ea5e6
Alexey Sokolov <alexvyber@gmail.com>
2023-02-07 16:00:34 +0300

    new stuff

---
+++ reg/src/branded-types.ts
@@ -0,0 +1,19 @@
+type EmailAddress = string & { __type: "EmailAddress" }
+
+const isEmailAddress = (str: string): str is EmailAddress => str.includes("@")
+
+function assertEmailAddress(str: string): asserts str is EmailAddress {
+  if (!str.includes("@")) throw new Error("str is not email")
+}
+
+const sendEmail = (_email: EmailAddress) => {}
+
+const signUp = (email: string) => {
+  if (isEmailAddress(email)) sendEmail(email)
+
+  sendEmail(email)
+
+  assertEmailAddress(email)
+
+  sendEmail(email)
+}
