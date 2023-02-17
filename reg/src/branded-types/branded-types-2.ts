79a08102fc45ea8551616c1c29a0a3176d6cc609
Alexey Sokolov <alexvyber@gmail.com>
2023-02-17 10:19:20 +0300

    new stuff

---
+++ reg/src/branded-types/branded-types-2.ts
@@ -0,0 +1,20 @@
+export type EmailAddress = string & { typename: "EmailAddress" }
+
+export function isValidEmail(email: string): email is EmailAddress {
+  const tester =
+    /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+
+  return tester.test(email)
+}
+
+export type AllLetters = string & {
+  typename: "AllLetters"
+}
+
+export function isAllLetters(char: unknown): char is AllLetters {
+  if (typeof char !== "string") {
+    return false
+  }
+
+  return /^[a-zA-Z]+$/.test(char)
+}
