090aa557a16a61b1dc9715ac443c835cf0f4c294
Alexey Sokolov <alexvyber@gmail.com>
2023-02-20 20:22:25 +0300

    generic stuff

---
+++ reg/src/generics.ts
@@ -0,0 +1,11 @@
+export {}
+
+const objKeys = <T extends object>(obj: T) => Object.keys(obj) as Array<keyof T>
+
+const obj = {
+  one: 1,
+  two: "two",
+  three: [1, 2, 3]
+}
+
+const resArr = objKeys(obj)
