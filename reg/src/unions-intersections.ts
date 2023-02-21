e286f47e87899eba05691d991cffb93a628b9a0e
Alexey Sokolov <alexvyber@gmail.com>
2023-02-21 10:04:52 +0300

    unions-intersections

---
+++ reg/src/unions-intersections.ts
@@ -0,0 +1,16 @@
+export {}
+
+type CatOwner = { catName: string }
+type DogOwner = { dogName: string }
+
+type HasCatOrDog = CatOwner | DogOwner
+
+const one: HasCatOrDog = { catName: "cat" }
+const two: HasCatOrDog = { dogName: "dog" }
+
+const three: HasCatOrDog = { ...one, ...two }
+const four: HasCatOrDog = { catName: "cat", dogName: "dog" }
+
+type HasCatAndDog = CatOwner & DogOwner
+
+const five: HasCatAndDog = { ...one, ...two }
