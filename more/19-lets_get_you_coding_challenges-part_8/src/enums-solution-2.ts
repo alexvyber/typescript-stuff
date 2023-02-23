f0290b4781db08c6408e71de26f55067cc816a30
Alexey Sokolov <alexvyber@gmail.com>
2023-02-23 11:15:52 +0300

    more stuff with Enum alternative

--- more/19-lets_get_you_coding_challenges-part_8/src/enums-solution-2.ts
+++ more/19-lets_get_you_coding_challenges-part_8/src/enums-solution-2.ts
@@ -30,3 +30,52 @@ function setCoursePrice(course: Products) {
 function courseWarning(course: never): never {
   throw new Error(`Course ${course} has not been priced`)
 }
+
+//
+
+type Account = {
+  accountType: AccountType
+  confirmed: boolean
+  other: any
+}
+
+const AccountTypes = {
+  Customer: "CUSTOMER",
+  Worker: "WORKER",
+  Contractor: "CONTRACTOR",
+} as const
+
+type AccountType = (typeof AccountTypes)[keyof typeof AccountTypes]
+type AccountTypeKeys = keyof typeof AccountTypes
+
+function setAccountType(account: Account, accountType: AccountTypeKeys): Account {
+  switch (accountType) {
+    case "Worker":
+      return {
+        ...account,
+        accountType: AccountTypes["Worker"],
+        confirmed: Math.random() > 0.5,
+      }
+
+    case "Customer":
+      return {
+        ...account,
+        accountType: AccountTypes["Customer"],
+        confirmed: Math.random() > 0.5,
+      }
+
+    case "Contractor":
+      return {
+        ...account,
+        accountType: AccountTypes["Contractor"],
+        confirmed: Math.random() > 0.5,
+      }
+
+    default:
+      accountError(accountType)
+  }
+}
+
+function accountError(accountType: never): never {
+  throw new Error(`You're trying to assign non-existent accountType: ${accountType} to the account`)
+}
