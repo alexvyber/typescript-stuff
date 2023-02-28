f4df5e8e891102241be14f1b497f3168ad08fbc5
Alexey Sokolov <alexvyber@gmail.com>
2023-02-28 10:04:55 +0300

    new stuff

---
+++ reg/src/const-array.ts
@@ -0,0 +1,30 @@
+const friends = ["Alex", "Ben", "Lynn"]
+
+console.log(
+  "🚀 ~ friends.hasOwnProperty('push'):",
+  friends.hasOwnProperty("push")
+) // false
+
+console.log("🚀 ~ Object.prototype:", Object.prototype)
+/*
+   constructor: ƒ Object()
+   hasOwnProperty: ƒ hasOwnProperty()
+   isPrototypeOf: ƒ isPrototypeOf()
+   propertyIsEnumerable: ƒ propertyIsEnumerable()
+   toLocaleString: ƒ toLocaleString()
+   toString: ƒ toString()
+   valueOf: ƒ valueOf()
+*/
+// or in
+/*
+  {} - in bun
+  [Object: null prototype] {} - in node or deno
+*/
+
+friends instanceof Array // true
+friends instanceof Object // true
+
+console.log(
+  "🚀 ~ friends.hasOwnProperty('push'):",
+  friends.hasOwnProperty("push")
+) // false
