c39c4b4cf1877fe070c1e5f809b0a87b126a9327
Alexey Sokolov <alexvyber@gmail.com>
2023-03-17 17:27:32 +0300

    stuff...

---
+++ ts-module-resolution-working/moduleResolution/Node/2-non-relative/example.d.ts
@@ -0,0 +1,3 @@
+import * as ts from "typescript";
+export declare function NonRelativePkg(): "file.d.ts";
+export declare function NonRelativeTS(): typeof ts;
---
+++ ts-module-resolution-working/moduleResolution/Node/7-typesVersions/example.d.ts
@@ -0,0 +1,1 @@
+export declare function TypesVersions(): "v5.d.ts";
