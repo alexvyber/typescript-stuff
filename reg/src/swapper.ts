75337f51d24af671f5577d7abcddcf7a4e8fed80
Alexey Sokolov <alexvyber@gmail.com>
2023-03-25 18:47:46 +0300

    new stuff

---
+++ reg/src/swapper.ts
@@ -0,0 +1,28 @@
+export class Swapper<T> {
+  constructor(private arr: T[]) {}
+
+  swap(one: number, two: number): this {
+    if (
+      one < 0 ||
+      one >= this.arr.length ||
+      two < 0 ||
+      two >= this.arr.length
+    ) {
+      return this
+    }
+
+    const temp = this.arr[one]
+    this.arr[one] = this.arr[two]
+    this.arr[two] = temp
+
+    return this
+  }
+
+  get array(): T[] {
+    return this.arr
+  }
+
+  log(): this {
+    return this
+  }
+}
