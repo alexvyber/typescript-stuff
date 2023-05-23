a9061188b3214c74985f610e3122d6292e24b134
Alexey Sokolov <alexvyber@gmail.com>
2023-05-23 11:58:10 +0300

    reg

--- more/16-typescript-basic-types/src/12-the-function-type.ts
+++ more/16-typescript-basic-types/src/12-the-function-type.ts
@@ -4,10 +4,11 @@ productName = (product: string): string => {
   return product
 }
 
-let productName1: (arg: string) => string
+export  let productName1: (arg: string) => string
 
-productName = arg => {
+productName = <T>(arg: T) => {
   return arg
 }
 
 console.log(productName("TS Bootcamp"))
+
